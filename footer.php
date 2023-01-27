c3a8154c93bb845981ab65c45e67e6f4ffdd406c
David McDonagh <david@leapfrogdbs.co.uk>
2023-01-27 12:35:57 +0000

    Responsive Styles

--- footer.php
+++ footer.php
@@ -16,23 +16,28 @@ $linkedin = get_field('linkedin', 'options');
 $youtube = get_field('youtube', 'options');
 $instagram = get_field('instagram', 'options');
 $imdb = get_field('imdb', 'options');
-$lfacebook = get_field('facebook', 'options');
+$facebook = get_field('facebook', 'options');
 
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<section class="bg-black text-white py-20">
+		<section class="bg-black text-white py-20 lg:py-28">
 			<div class="container">
-				<div class="row">
-					<div class="col">
-						<h3 class="headingThree">Join Our Newsletter</h3>
-						<p class="bodyText">Email Address Here</p>
-						<p class="bodyText">Sign Up</p>
+				<div class="row pb-8 border-b border-white lg:pb-14">
+					<div class="col lg:grid grid-cols-12 lg:items-center lg:gap-x-4">
+						<h3 class="bodyText font-gothic-bold lg:col-span-3 lg:mb-0 lg:text-left">Join Our Newsletter</h3>
+						<form action="" class="flex flex-col md:flex-row gap-y-3 gap-x-4 justify-center lg:col-span-6">
+							<input type="text" name="email" id="email" placeholder="Email address here" class="bg-black text-white rounded-none border-white text-center bodyText mb-0 py-2 md:w-64">
+							<input type="submit" value="Sign up" class="bg-black text-white rounded-none border-white text-center bodyText mb-0 py-2 md:w-64">
+						</form>
+							<p class="hidden subtitleTwo lg:bodyText lg:col-span-3 lg:mb-0 lg:text-right lg:block">&copy;<?php echo date("Y"); ?> by Whitelock Design Limited.</p>
 					</div>
+									
+					
 				</div>
 			
-				<div class="row">
-					<div class="col flex gap-x-2 justify-center">
+				<div class="row mt-8 lg:items-center lg:mt-14">
+					<div class="col flex gap-x-2 justify-center lg:col-span-6 lg:justify-end lg:order-2">
 						<?php if($linkedin) { ?>
 							<a href="<?php echo $linkedin['url']; ?>"><img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/img/social-media/linkedin.svg" class="w-8 h-auto" alt=""></a>
 						<?php } ?>
@@ -49,11 +54,11 @@ $lfacebook = get_field('facebook', 'options');
 							<a href="<?php echo $facebook['url']; ?>"><img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/img/social-media/fb.svg" class="w-8 h-auto" alt=""></a>
 						<?php } ?>
 					</div>
-					<div class="col mt-6">
-						<img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/img/white-logo.png"  class="mx-auto" alt="">
+					<div class="col mt-6 lg:col-span-6 lg:mt-0">
+						<img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/img/white-logo.png"  class="w-16 h-auto mx-auto lg:ml-0 lg:order-1" alt="">
 					</div>
-					<div class="col mt-6">
-							<p class="subtitleTwo">&copy; Copyright <?php echo date("Y"); ?>by Whitelock Design Limited.</p>
+					<div class="col mt-6 lg:hidden">
+							<p class="subtitleTwo">&copy;<?php echo date("Y"); ?> by Whitelock Design Limited.</p>
 					</div>
 				</div>
 			</div>
