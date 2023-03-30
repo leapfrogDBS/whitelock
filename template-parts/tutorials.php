1e31bed1016ddc068318b4ae57a6cc70d10ebba4
David McDonagh <david@leapfrogdbs.co.uk>
2023-03-30 13:23:45 +0100

    UIpdates

--- template-parts/tutorials.php
+++ template-parts/tutorials.php
@@ -41,16 +41,16 @@
                         
 
                         ?>
-                        <div class="col bg-whitlockBlue text-white rounded-2xl mb-12 lg:grid lg:grid-cols-12">
-                            <p class="bodyText my-1 pt-1 font-gothic-bold lg:hidden"><?php echo sprintf("%02d", $count); ?></p>
-                            <div style="background-image: url(<?php echo $tutorials_image['sizes']['large']; ?>)" class="order-2 lg:col-span-8 bg-cover bg-center lg:rounded-r-2xl h-72 lg:h-full" id="tutorial-image"></div>
-                            <div id="blurb" class="px-6 py-8 pb-10 order-1 lg:col-span-4 lg:p-0 lg:flex lg:flex-col lg:justify-between">
-                                <p class="hidden bodyText py-3 font-gothic-bold lg:block lg:border-b lg:border-white"><?php echo sprintf("%02d", $count); ?></p>
-                                <div class="lg:px-6 lg:text-left">
-                                    <h2 class="headingTwo uppercase leading-none lg: py-6"><?php echo $tutorials_title; ?></h2>
-                                    <p class="bodyText mb-8 lg:pb-6"><?php echo $tutorials_info; ?></p>
+                        <div class="col bg-whitlockBlue text-white rounded-2xl mb-12 xl:grid xl:grid-cols-12">
+                            <p class="bodyText my-1 pt-1 font-gothic-bold xl:hidden"><?php echo sprintf("%02d", $count); ?></p>
+                            <div style="background-image: url(<?php echo $tutorials_image['sizes']['large']; ?>)" class="order-2 xl:col-span-8 bg-cover bg-center xl:rounded-r-2xl h-72 xl:h-full" id="tutorial-image"></div>
+                            <div id="blurb" class="px-6 py-8 pb-10 order-1 xl:col-span-4 xl:p-0 xl:flex xl:flex-col xl:justify-between">
+                                <p class="hidden bodyText py-3 font-gothic-bold xl:block xl:border-b xl:border-white"><?php echo sprintf("%02d", $count); ?></p>
+                                <div class="xl:px-6 xl:text-left">
+                                    <h2 class="headingTwo uppercase leading-none xl: py-6"><?php echo $tutorials_title; ?></h2>
+                                    <p class="bodyText mb-8 xl:pb-6"><?php echo $tutorials_info; ?></p>
                                 </div>
-                                <div class="lg:border-t lg:border-white lg:py-6 lg:text-left lg:px-6 flex justify-between items-center">
+                                <div class="xl:border-t xl:border-white xl:py-6 xl:text-left xl:px-6 flex justify-between items-center">
                                     <a class="cta-button cta-button-white-outline whitespace-nowrap mr-6" href="<?php echo $tutorials_link['url']; ?>">Buy now</a>
                                     <h3 class="headingThree uppercase leading-none"><?php echo $tutorials_price; ?></h3>
                                 </div>
